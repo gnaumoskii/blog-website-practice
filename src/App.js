a267b1edf0b8be09290dba5331d455aeb1974e2f
Gjorgji <gjorgjinaumoski@hotmail.com>
2022-05-07 16:18:38 +0200

    Adding app components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Footer from './Footer';
+
+import Home from './Home';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import About from './About';
+
+import Missing from './Missing';
+
+import { Route, Switch, useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function App() {
   return (
     <div className="App">
-      
+     asdas
     </div>
   );
 }
